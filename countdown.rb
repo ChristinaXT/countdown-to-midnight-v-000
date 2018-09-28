5e70be06ce42d468e687d787f9f8459c60410bd3
Christina Williams <christinaxtwilliams@gmail.com>
2018-09-28 18:57:06 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,9 @@ def countdown(num)
   while num > 0 
   puts "#{num} SECOND(S)!"
   num -=1 
-  
+  end
+  	"HAPPY NEW YEAR!"
+ end
 def countdown_with_sleep(num)
   while num > 0 
   puts "#{num} SECOND(S)!"
